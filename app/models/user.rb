f069ff4a6ac3d71887e9b9a5aa2a43739024e645
黄志成 <huangzhicheng@oschina.cn>
2019-12-27 11:31:24 +0800

    make a basic user model(including secure password)

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,9 @@ class User < ApplicationRecord
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: true
 
+  validates :password, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
+
+
 end
